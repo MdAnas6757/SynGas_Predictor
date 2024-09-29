fd949ca29d9156441c8f9258bd20761f016e0108
Md Anas <97391971+MdAnas6757@users.noreply.github.com>
2024-09-29 17:51:06 +0530

    Updated Code

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,13 @@ from flask import Flask, request, jsonify
 from model import predict_syngas_yield_all
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../frontend/build", static_url_path="")
 CORS(app)  # Enable CORS for frontend-backend communication
+
+# Serve the React frontend
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
